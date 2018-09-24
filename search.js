1059d1a719963e6cc38ca65774db714ab8e9df42
Joao Lopes <joao.lopes@simplesurance.de>
2018-09-24 14:21:36 +0200

    Implement search

--- search.js
+++ search.js
@@ -1,11 +1,21 @@
 const $nameSearch = $('#name-search')
 const $nameResults = $('#name-search-results')
+const $jobSearch = $('#job-search')
+const $jobResults = $('#job-search-results')
 
-function filter(list = [], needle = '', keys) {
+function filterAndSort(list = [], needle = '', keys) {
   return list.filter(value => {
-    return keys.find(key => {
-      return value[key].toLowerCase().includes(needle.toLowerCase())
-    })
+    if (keys) {
+      return keys.find(key => {
+        return value[key].toLowerCase().includes(needle.toLowerCase())
+      })
+    } else {
+      return value.toLowerCase().includes(needle.toLowerCase())
+    }
+  }).sort((a, b) => {
+    return a.name > b.name ? 1
+    : a.name < b.name ? -1
+    : 0
   })
 }
 
@@ -22,6 +32,15 @@ function toOptions(list) {
     return $option
   })
 }
+function toJobResults(list) {
+  return list.map(person => {
+    const $option = document.createElement('div')
+    $option.className = "result"
+    $option.setAttribute('data-value', person.username)
+    $option.innerHTML = `<small>${person.name}</small><br/><span>${person.position}</span>`
+    return $option
+  })
+}
 
 on($nameSearch, 'focus', e => {
   $nameResults.classList.remove('hidden')
@@ -31,8 +50,19 @@ on($nameSearch, 'blur', e => {
 })
 on($nameSearch, 'input', e => {
   if (e.target.value.length > 0) {
-    results = filter(people, e.target.value, ['name', 'username'])
+    results = filterAndSort(people, e.target.value, ['name', 'username'])
     $nameResults.innerHTML = ''
     toOptions(results).forEach($result => $nameResults.append($result))
+  } else {
+    $nameResults.innerHTML = ''
+  }
+})
+on($jobSearch, 'input', e => {
+  if (e.target.value.length > 0) {
+    results = filterAndSort(people, e.target.value, ['position'])
+    $jobResults.innerHTML = ''
+    toJobResults(results).forEach($result => $jobResults.append($result))
+  } else {
+    $jobResults.innerHTML = ''
   }
 })
