f3cbcebece40d61857e5b9ee09fdcb1b13997d6e
João Lopes <lopis@users.noreply.github.com>
2018-10-23 18:20:58 +0200

    Implement Google Login (#2)
    * implement google login and fetch from spreadsheet
    
    * remove public user data
    
    * add clicking on tables

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
+// Column indexes
+const ROW = 0
+const SECTION = 1
+const TABLE = 2
+const NAME = 3
+const ROLE = 4
+const HIPCHAT = 5
 
-fetch('data.json')
-.then(data => data.json())
-.then(data => {
-  console.log(data)
-  data.people.forEach(person => {
+function listMajors() {
+  gapi.client.sheets.spreadsheets.values.get({
+    spreadsheetId: '1Mf6l4NasmHx8FKlrhTU_am4bdMzRu5yC1yMQ6PqG1RQ',
+    range: 'User Data!A2:F',
+  }).then(function(response) {
+    const range = response.result
+    const people = []
+
+    if (range.values.length > 0) {
+      range.values.forEach(row => {
+        if (row[HIPCHAT] && row[HIPCHAT] !== '-') {
+          people.push({
+            username: row[HIPCHAT],
+            name: row[NAME],
+            position: row[ROLE],
+            address: `${row[ROW]}.${row[SECTION]}.${row[TABLE]}`,
+            responsabilities: [],
+          })
+        }
+      })
+      setPeople(people)
+    }
+  }, function(response) {
+    appendPre('Error: ' + response.result.error.message);
+  });
+}
+
+window.addEventListener('signInSuccess', function () {
+  listMajors()
+})
+
+function setPeople (people) {
+  console.log(people)
+  people.forEach(person => {
     const $option = document.createElement('option')
     $option.setAttribute('value', person.username)
     $option.innerText = person.name
     $("#name-options").appendChild($option)
   })
-  window.people = data.people
-})
+  window.people = people
+}
